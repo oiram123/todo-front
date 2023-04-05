47f874acf10a79a3aa5ad38bbdbf379fae014cfd
oiram123 <margjinimario@gmail.com>
2023-04-05 17:56:25 +0200

    updates

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,16 +1,30 @@
 import { LockClosedIcon } from "@heroicons/react/20/solid";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const Login = async () => {
+    const userParams = {
+      email,
+      password,
+    }
+    try {
+      console.log()
+      const res = await axios.post(process.env.AUTH + "login", userParams);
+      console.log(res)
+      navigate("/home");
+    } catch (error) {
+      console.log("err", error);
+    }
+  };
+
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8 bg-lightgray">
         <div className="w-full max-w-md space-y-8">
           <div>
@@ -44,6 +58,7 @@ export default function Login() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                   className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Email address"
@@ -58,6 +73,7 @@ export default function Login() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Password"
@@ -79,6 +95,7 @@ export default function Login() {
             <div>
               <button
                 type="submit"
+                onClick={Login}
                 className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
