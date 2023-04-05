4b870743aafb976e987973befe41aac3541e8d34
oiram123 <bishainter78@yahoo.com>
2023-04-05 19:47:40 +0200

    adding edit button

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,6 +45,8 @@ export default function Home() {
     // setItems(newItems);
   };
 
+
+
   return (
     <>
       {/* Background color split screen for large screens */}
@@ -80,6 +82,148 @@ export default function Home() {
         </div>
 
         {/* 3 column wrapper */}
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <div className="mx-auto w-full max-w-7xl flex-grow lg:flex xl:px-8">
+            {/* Left sidebar & main wrapper */}
+            <div className="min-w-0 flex-1 bg-gray-200 xl:flex">
+              <div className="bg-white lg:min-w-0 lg:flex-1 border-r border-gray-400">
+                <h2 className="text-2xl font-bold px-6 py-2 bg-gray-800 text-white">
+                  To Do
+                </h2>
+                <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
+                  {/* To Do area */}
+                  <Droppable droppableId="to-do">
+                    {(provided) => (
+                      <div ref={provided.innerRef} {...provided.droppableProps}>
+                        {toDoItems.map((item, index) => (
+                          <Draggable
+                            key={item._id}
+                            draggableId={item.status}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                className="bg-white rounded-lg shadow-lg border border-gray-400 mb-4 p-4 text-gray-800"
+                              >
+                                <h2 className="text-lg font-bold mb-2">
+                                  {item.title}
+                                </h2>
+                                <p className="mb-4">{item.content}</p>
+                                <div className="flex justify-between">
+                                  <button className="block w-20 justify-center px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Delete
+                                  </button>
+                                  <button className="block w-20 justify-right px-4 py-2 bg-blue-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Edit
+                                  </button>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
+              </div>
+              <div className="bg-white lg:min-w-0 lg:flex-1 border-r border-gray-400">
+                <h2 className="text-2xl font-bold px-6 py-2 bg-gray-800 text-white">
+                  Doing
+                </h2>
+
+                <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
+                  {/* Doing area */}
+                  <Droppable droppableId="doing">
+                    {(provided) => (
+                      <div ref={provided.innerRef} {...provided.droppableProps}>
+                        {doingItems.map((item, index) => (
+                          <Draggable
+                            key={item._id}
+                            draggableId={item.status}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                className="bg-white rounded-lg shadow-lg border border-gray-400 mb-4 p-4 text-gray-800"
+                              >
+                                <h2 className="text-lg font-bold mb-2">
+                                  {item.title}
+                                </h2>
+                                <p className="mb-4">{item.content}</p>
+                                <div className="flex justify-between">
+                                  <button className="block w-20 justify-center px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Delete
+                                  </button>
+                                  <button className="block w-20 justify-right px-4 py-2 bg-blue-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Edit
+                                  </button>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
+              </div>
+
+              <div className="bg-white lg:min-w-0 lg:flex-1">
+                <h2 className="text-2xl font-bold px-6 py-2 bg-gray-800 text-white">
+                  Done
+                </h2>
+
+                <div className="h-full py-6 px-4 sm:px-6 lg:px-8">
+                  {/* Done area */}
+                  <Droppable droppableId="done">
+                    {(provided) => (
+                      <div ref={provided.innerRef} {...provided.droppableProps}>
+                        {doneItems.map((item, index) => (
+                          <Draggable
+                            key={item._id}
+                            draggableId={item.status}
+                            index={index}
+                          >
+                            {(provided) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                className="bg-white rounded-lg shadow-lg border border-gray-400 mb-4 p-4 text-gray-800"
+                              >
+                                <h2 className="text-lg font-bold mb-2">
+                                  {item.title}
+                                </h2>
+                                <p className="mb-4">{item.content}</p>
+                                <div className="flex justify-between">
+                                  <button className="block w-20 justify-center px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Delete
+                                  </button>
+                                  <button className="block w-20 justify-right px-4 py-2 bg-blue-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+                                    Edit
+                                  </button>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>
+              </div>
+            </div>
+          </div>
+        </DragDropContext>
       </div>
     </>
   );
