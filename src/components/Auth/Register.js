451d2c891edc856732155ce72e12aa26daeb6a4e
oiram123 <margjinimario@gmail.com>
2023-04-06 18:19:07 +0200

    updates

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,6 +1,33 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import AuthContext from "./AuthContext";
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+
+
+  const Register = async (event) => {
+    event.preventDefault()
+    const userParams = {
+      email,
+      password,
+    }
+    try {
+      console.log(userParams)
+     const res = await axios.post('https://todo-app-1u8v.onrender.com/api/v1/auth/register', userParams);
+     const {user, token} = res.data
+      login(user, token)
+      navigate("/home");
+    } catch (error) {
+      console.log("err", error);
+    }
+  };
+
   return (
     <>
       {/*
@@ -54,6 +81,7 @@ export default function Register() {
                   id="password"
                   name="password"
                   type="password"
+                  onChange={(e) => setPassword(e.target.value)}
                   autoComplete="current-password"
                   required
                   className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -87,6 +115,7 @@ export default function Register() {
             <div>
               <button
                 type="submit"
+                onClick={Register}
                 className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
